71569841301ccf2b9beea9df631cba2b5e20dd96
GabrielOnohara <gabakio11@gmail.com>
2023-08-14 00:34:23 -0300

    usando timeout com sendrResponse

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,7 +1,6 @@
 
 /* global chrome */
-//Observacao sempre que for necessário responder uma mensagem que não seja assíncrona use o return true após o sendResponse
-let rawContentBody 
+//Observacao sempre que for necessário responder uma mensagem que não seja assíncrona use o return true
 
 //funcao que verifica conexao
 const port = chrome.runtime.connect({ name: "contentScript" });
@@ -11,65 +10,58 @@ port.onMessage.addListener(response => {
     console.log("Resposta do background script:", response.data);
 });
 
-function mountLoadingDOM(){
-  let loadingInnerHTML = `
-        <div class= "phobia-container">
-          <h1 class= "phobia-title">Verificando imagens sensíveis..</h1>
-        </div>
-        <style>
-          .phobia-container {
-            min-height: 100vh; 
-            min-width: 100vw;
-            background: #6C7A89;
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            font-family: Arial, Helvetica, sans-serif;
-            font-style: normal;
-            z-index: 999999999999999999999999;
-            position: fixed;
-            top: 0;
-            left: 0;
-          }
-          .phobia-title {
-            font-size: 32px;
-            color: #fff;
-            text-align: center
-          }
-        </style>
-      ` 
-      // let rawContentBody = document.body.innerHTML;
-      // let imgs  = document.body.querySelectorAll('img')
-      
-      //salva dom antigo para voltar com ele processado
-      //altera o dom com uma mensagem de aviso 
-      try {
-        rawContentBody = document.body.innerHTML
-        document.body.innerHTML = loadingInnerHTML
-  
-        const imgs = rawContentBody.querySelectorAll('img')
-        imgs.forEach(img => {
+function mountLoadingDOM() {
+  try {
+      // Create and add the warning message container
+      const loadingContainer = document.createElement('div');
+      loadingContainer.className = 'phobia-container';
+      loadingContainer.innerHTML = `
+          <h1 class="phobia-title">Verificando imagens sensíveis..</h1>
+      `;
+
+      document.body.appendChild(loadingContainer);
+
+      // Apply the background style
+      const styleElement = document.createElement('style');
+      styleElement.textContent = `
+        .phobia-container {
+          min-height: 100vh; 
+          min-width: 100vw;
+          background: #6C7A89;
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
+          font-family: Arial, Helvetica, sans-serif;
+          font-style: normal;
+          z-index: 999999999999999999999999;
+          position: fixed;
+          top: 0;
+          left: 0;
+        }
+        .phobia-title {
+          font-size: 32px;
+          color: #fff;
+          text-align: center
+        }
+      `;
+      document.head.appendChild(styleElement);
+
+      // Apply the blur to the images
+      const imgs = document.querySelectorAll('img');
+      imgs.forEach(img => {
           img.style.filter = "blur(20px)";
-        });
-        
-        document.body.innerHTML = rawContentBody
-        
+      });
+      setTimeout(()=> {
+        document.body.removeChild(loadingContainer);
 
-      // const body = document.querySelector('body');
-      // body.insertAdjacentHTML('afterbegin', loadingInnerHTML);    
-      // body.append('afterbegin', loadingInnerHTML);   
-      // Insira o conteúdo no início do <body>
-      // body.insertAdjacentHTML('afterbegin', loadingInnerHTML);
-
-      } catch (error) {
-        //caso gere uma excecao retorna falso
-        return false
-      }finally {
-        
-      //retorna verdadeiro se realizou todas as operacoes
-        return true
-      }
+      }, 5000)
+      // Return a function to remove the changes
+      return true
+  } catch (error) {
+      console.error(error);
+      return null;
+  }
 }
 
 //funcao que ouve as mensagens e aplica a funcao de acordo com seus conteúdos
