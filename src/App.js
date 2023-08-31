d757dda3f6bd9f3a6a128bbe65500b36446027c4
GabrielOnohara <gabakio11@gmail.com>
2023-08-31 19:54:29 -0300

    Montando estrutura  da extensao

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 /* global chrome */
-import React from 'react';
-
+import React, { 
+  
+ } from 'react';
+import './App.css'
+import Toggle from 'react-styled-toggle';
 function App() {
-  const manipulateDOM = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, { action: 'manipulateDOM' }, response => {
-        if (response.success) {
-          console.log('DOM manipulation successful');
-        }
-      });
-    });
-  };
+  // const manipulateDOM = () => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+  //     const activeTab = tabs[0];
+  //     chrome.tabs.sendMessage(activeTab.id, { action: 'manipulateDOM' }, response => {
+  //       if (response.success) {
+  //         console.log('DOM manipulation successful');
+  //       }
+  //     });
+  //   });
+  // };
 
   React.useEffect(() => {
     // Add event listener for tab updates
@@ -28,9 +31,41 @@ function App() {
     // };
   }, []);
 
+  const [aracnofobia, setAracnofobia] = React.useState(true)
+  const [tripofobia, setTripofobia] = React.useState(true)
+  const [coulrofobia, setCoulrofobia] = React.useState(true)
+
   return (
     <div className="App">
-      <button onClick={manipulateDOM}>Manipulate DOM</button>
+      <h1>PhobIA Extension 😱</h1>
+      <p>Extensão de detecção de fotos sensíveis para fobias</p>
+      <ul>
+        <li>
+          <h3>Aracnofobia</h3> 
+          {
+            <Toggle checked={aracnofobia}
+              onChange={() => setAracnofobia((oldValue) => !oldValue)}
+            />
+          }
+        </li>
+        <li>
+        <h3>Tripofobia</h3> 
+          {
+            <Toggle checked={tripofobia}
+              onChange={() => setTripofobia((oldValue) => !oldValue)}
+            />
+          }
+        </li>
+        <li>
+        <h3>Coulrofobia</h3>  
+          {
+            <Toggle checked={coulrofobia}
+              onChange={() => setCoulrofobia((oldValue) => !oldValue)}
+            />
+          }
+        </li>
+      </ul>
+      <a href='https://github.com/GabrielOnohara/phobia-extension-app/' target="_blank" rel='noreferrer'> Github do Projeto</a>
     </div>
   );
 }
